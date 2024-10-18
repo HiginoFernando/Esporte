e84271504501c987cd015b383ddcbcfe9f265862
Rytchelle <ratkarb12@gamil.com>
2024-10-18 11:24:35 -0300

    V2

--- src/main/java/com/esporte/DTO/esporte_DTO.java
+++
@@ -1,5 +0,0 @@
-package com.esporte.DTO;
-
-public record esporte_DTO(String Nome, int num_jogador, String local, String descrição) {
-
-}
---
+++ src/main/java/com/esporte/demo/controller/UsuarioDTO.java
@@ -0,0 +1,5 @@
+package com.esporte.demo.controller;
+
+public record UsuarioDTO(String Nome, int num_jogador, String local, String descrição) {
+
+}
